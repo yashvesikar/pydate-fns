a21179dfeaf52832553680d21a1c8f3fb697a088
yashvesikar <yashvesikar@gmail.com>
2022-08-21 13:59:36 -0700

    - adding to_date function - adding pytest to dev dependencies

---
+++ src/core/to_date/to_date.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from decimal import Decimal
+from typing import Union
+
+
+def to_date(dirty_date: Union[int, float, Decimal]) -> datetime:
+    """
+    Convert a dirty date to a datetime object.
+    :param dirty_date: The dirty date to convert.
+    :return: The datetime object.
+    """
+
+    if isinstance(dirty_date, int) or isinstance(dirty_date, float):
+        return datetime.fromtimestamp(dirty_date)
+    elif isinstance(dirty_date, Decimal):
+        return datetime.fromtimestamp(float(dirty_date))
